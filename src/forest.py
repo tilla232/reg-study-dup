9d4af77f20ec27301a0aa2b022b0833668f2e394
RetepAdam <peter.a.nygaard@gmail.com>
2017-04-28 16:25:15 -0600

    Oh well.

--- src/forest.py
+++ src/forest.py
@@ -8,18 +8,26 @@ from sklearn.preprocessing import StandardScaler
 if __name__ == '__main__':
     df = pd.read_csv('../data/Train.csv')
     X = cd.mister_clean(df)
+    WL,SSL,TEX,BL,TTT,MG = cd.split_groups(X)
+    WL_dummies = cd.get_dummies(WL)
+    SSL_dummies = cd.get_dummies(SSL)
+    TEX_dummies = cd.get_dummies(TEX)
+    BL_dummies = cd.get_dummies(BL)
+    TTT_dummies = cd.get_dummies(TTT)
+    MG_dummies = cd.get_dummies(MG)
+
     df2 = pd.read_csv('../data/Train.csv')
-    y = df2[['SalePrice']]
+    y = df2[['SalePrice','ProductGroup']]
+    BL_y = np.array(y[y['ProductGroup'] == 'BL'])
     # cd.make_dummies(df['Ride_Control'])
     # cd.make_dummies(df['Forks'])
     # cd.make_dummies(df['Coupler_System'])
     # cd.make_dummies(df['Grouser_Tracks'])
-    # WL,SSL,TEX,BL,TTT,MG = cd.split_groups(X)
     # cd.delete_empty_columns([WL, SSL, TEX, BL, TTT, MG])
     #
     # BL_y = np.array(BL[BL.columns[0]])
     # BL_X = np.array(BL[BL.columns[1:]])
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    X_train, X_test, y_train, y_test = train_test_split(BL_dummies, BL_y)
 
     rf = RandomForestClassifier()
     rf.fit(X_train,y_train)
